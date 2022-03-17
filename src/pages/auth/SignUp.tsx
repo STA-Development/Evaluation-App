c6a3c95afe1c4d88bf435593cc1589b3371037c3
yavetisyan <y.avetisyan88@gmail.com>
2022-03-17 19:39:22 +0400

    FEAT: fix icons

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 
 const SignUp = () => {
-    return (
-        <Box className="auth__title">
-            <h2>Sign Up</h2>
-            <p>Already have an account? </p>
-            <button>Sgn In</button>
-            <input type="text"/>
-        </Box>
-    );
+  return (
+    <Box className="auth__title">
+      <h2>Sign Up</h2>
+      <p>Already have an account? </p>
+      <button>Sgn In</button>
+      <input type="text" />.
+    </Box>
+  );
 };
 
 export default SignUp;
