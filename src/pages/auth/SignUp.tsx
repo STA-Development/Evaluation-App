33657094feda19ee6744b484a4b23681d8f33823
YuraAvetisyan <y.avetisyan88@gmail.com>
2022-03-18 21:55:04 +0400

    FEAT: congit signup box

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,47 +1,49 @@
-import React, {useEffect} from "react";
-import {Box, Button, FormControlLabel, FormGroup, TextField, Typography, useTheme,} from "@mui/material";
-import {useStyles} from "../../assets/styles";
+import React from "react";
+import {Box, Button, FormControlLabel, FormGroup, TextField, Typography,} from "@mui/material";
 import Checkbox from "@mui/material/Checkbox";
+import {useStyles} from "../../assets/scssInJS/signUp";
 
 const SignUp = () => {
-    const classes = useStyles();
-    const theme = useTheme();
-    console.log(theme);
-    // @ts-ignore
-    const {palette} = theme;
+    const classes = useStyles()
 
-    useEffect(() => {
-        console.log(palette.primary.main, 88);
-    }, [palette.primary.main]);
     return (
         <Box className="auth__title">
             <Box component="div" className="auth__title-text">
-                <Typography variant="h2" className={classes.authText}>
+                <Typography variant="h2" className={classes.authHeader}>
                     Sign up
                 </Typography>
                 <Box>
-                    <Typography className="auth__inner-text">
-                        Already have an account?{" "}
+                    <Typography className={classes.authText}>
+                        Already have an account?
                     </Typography>
-                    <a>Sign in</a>
+
+                    <Button variant="text">Sign in</Button>
                 </Box>
             </Box>
             <FormGroup>
-                <TextField label="Name / Surname" variant="outlined" fullWidth/>
-                <TextField label="Email" variant="outlined" type="email" fullWidth/>
-                <TextField
-                    label="Password (6+ charachter , 1 capital letter, 1 number)"
-                    type="password"
-                    variant="outlined"
-                    fullWidth
-                />
+                <Box className='auth__input'>
+                    <TextField label="Name / Surname" variant="outlined"
+                               className='auth__input-box' size='small'
+                               fullWidth/>
+                    <TextField label="Email" variant="outlined" type="email" fullWidth size='small'/>
+                    <TextField
+                        className={classes.authInput}
+                        label="Password (6+ charachter , 1 capital letter, 1 number)"
+                        type="password"
+                        variant="outlined"
+                        fullWidth
+                        size='small'
+                    />
+                </Box>
+
                 <FormControlLabel
                     control={<Checkbox/>}
                     label="Keep me signed in"
                     value="checkbox"
+                    className={classes.authCheck}
                 />
 
-                <Button variant="contained" color="primary">Sign Up</Button>
+                <Button variant="contained" size='large'>Sign Up</Button>
             </FormGroup>
         </Box>
     );
