972abdd85337ca9c71441565d137d6b8837740c3
gor017 <gorkhachatryan017@gmail.com>
2022-03-18 21:09:36 +0400

    FEAT: corrected background images

---
+++ src/assets/images/auth/AuthBackgroundVector.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AuthBackgroundVector = () => {
+  return (
+    <svg
+      width="837"
+      height="1024"
+      viewBox="0 0 837 1024"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0 1027L426.306 0H845V1027H0Z" fill="#E0FAFD" />
+    </svg>
+  );
+};
+
+export default AuthBackgroundVector;
