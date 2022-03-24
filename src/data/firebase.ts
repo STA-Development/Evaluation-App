9fcd14499c912b8fa739aa58da6acbd93d8c30de
yavetisyan <y.avetisyan88@gmail.com>
2022-03-24 16:57:28 +0400

    FEAT: add firebase

---
+++ src/data/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1ERAEfRI5MujwltzKGmULTCZx5IPNCvc",
+  authDomain: "evaluation-513c5.firebaseapp.com",
+  projectId: "evaluation-513c5",
+  storageBucket: "evaluation-513c5.appspot.com",
+  messagingSenderId: "948069959400",
+  appId: "1:948069959400:web:d8884376352ea97e17eb5d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -52,7 +52,7 @@ const SignUp = () => {
       setEmailError(false);
       setPasswordError(false);
 
-      dispatch(setUser({ name, email, password }));
+      dispatch(setUser({name, email, password}));
 
       setEmail("");
       setName("");
@@ -69,107 +69,108 @@ const SignUp = () => {
     }
   };
 
+
   return (
-    <div>
-      <Box>
-        <Grid
-          container
-          className="auth"
-          justifyContent="space-around"
-          alignItems="center"
-        >
-          <Grid item md={5} sm={6} xs={12} alignItems="center">
-            <Paper className="auth__title ">
-              <Box component="div" className="auth__title-text">
-                <Typography
-                  variant="h2"
-                  className={classes.authHeader}
-                  gutterBottom
-                >
-                  Sign up
-                </Typography>
-                <Box>
-                  <Typography className={classes.authText}>
-                    Already have an account?
-                  </Typography>
-
-                  <Button variant="text">Sign in</Button>
-                </Box>
-              </Box>
-              <FormGroup>
-                <Box
-                  component="form"
-                  noValidate
-                  className="auth__input-box"
-                  onSubmit={handleSubmit}
-                >
-                  <TextField
-                    InputLabelProps={{ style: { fontSize: 14 } }}
-                    className={classes.authInput}
-                    label="Name / Surname"
-                    variant="outlined"
-                    size="small"
-                    fullWidth
-                    value={name}
-                    error={nameError}
-                    onChange={getName}
-                    autoComplete="name"
-                  />
-                  <TextField
-                    InputLabelProps={{ style: { fontSize: 14 } }}
-                    className={classes.authInput}
-                    label="Email"
-                    variant="outlined"
-                    type="email"
-                    fullWidth
-                    error={emailError}
-                    value={email}
-                    size="small"
-                    onChange={getEmail}
-                    autoComplete="email"
-                  />
-                  <TextField
-                    InputLabelProps={{ style: { fontSize: 14 } }}
-                    className={classes.authInput}
-                    label="Password (6+ charachter , 1 capital letter, 1 number)"
-                    type="password"
-                    variant="outlined"
-                    fullWidth
-                    error={paswordError}
-                    value={password}
-                    size="small"
-                    onChange={getPassword}
-                    autoComplete="new-password"
-                  />
-
-                  <FormControlLabel
-                    control={<Checkbox />}
-                    label="Keep me signed in"
-                    value="checkbox"
-                    className={classes.authCheck}
-                  />
-                  <Button type="submit" variant="contained" size="large">
-                    Sign Up
-                  </Button>
-                </Box>
-              </FormGroup>
-            </Paper>
-          </Grid>
+      <div>
+        <Box>
           <Grid
-            item
-            md={7}
-            sm={6}
-            xs={12}
-            justifyContent="flex-end"
-            style={{ display: "flex" }}
+              container
+              className="auth"
+              justifyContent="space-around"
+              alignItems="center"
           >
-            <Box className="auth__box-right ">
-              <SignUpImg />
-            </Box>
+            <Grid item md={5} sm={6} xs={12} alignItems="center">
+              <Paper className="auth__title ">
+                <Box component="div" className="auth__title-text">
+                  <Typography
+                      variant="h2"
+                      className={classes.authHeader}
+                      gutterBottom
+                  >
+                    Sign up
+                  </Typography>
+                  <Box>
+                    <Typography className={classes.authText}>
+                      Already have an account?
+                    </Typography>
+
+                    <Button variant="text">Sign in</Button>
+                  </Box>
+                </Box>
+                <FormGroup>
+                  <Box
+                      component="form"
+                      noValidate
+                      className="auth__input-box"
+                      onSubmit={handleSubmit}
+                  >
+                    <TextField
+                        InputLabelProps={{style: {fontSize: 14}}}
+                        className={classes.authInput}
+                        label="Name / Surname"
+                        variant="outlined"
+                        size="small"
+                        fullWidth
+                        value={name}
+                        error={nameError}
+                        onChange={getName}
+                        autoComplete="name"
+                    />
+                    <TextField
+                        InputLabelProps={{style: {fontSize: 14}}}
+                        className={classes.authInput}
+                        label="Email"
+                        variant="outlined"
+                        type="email"
+                        fullWidth
+                        error={emailError}
+                        value={email}
+                        size="small"
+                        onChange={getEmail}
+                        autoComplete="email"
+                    />
+                    <TextField
+                        InputLabelProps={{style: {fontSize: 14}}}
+                        className={classes.authInput}
+                        label="Password (6+ charachter , 1 capital letter, 1 number)"
+                        type="password"
+                        variant="outlined"
+                        fullWidth
+                        error={paswordError}
+                        value={password}
+                        size="small"
+                        onChange={getPassword}
+                        autoComplete="new-password"
+                    />
+
+                    <FormControlLabel
+                        control={<Checkbox/>}
+                        label="Keep me signed in"
+                        value="checkbox"
+                        className={classes.authCheck}
+                    />
+                    <Button type="submit" variant="contained" size="large">
+                      Sign Up
+                    </Button>
+                  </Box>
+                </FormGroup>
+              </Paper>
+            </Grid>
+            <Grid
+                item
+                md={7}
+                sm={6}
+                xs={12}
+                justifyContent="flex-end"
+                style={{display: "flex"}}
+            >
+              <Box className="auth__box-right ">
+                <SignUpImg/>
+              </Box>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
-    </div>
+        </Box>
+      </div>
   );
 };
 
