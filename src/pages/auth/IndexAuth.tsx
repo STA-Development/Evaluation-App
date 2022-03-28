e2e740a5a5fb625e8976d5fbce466d81175248a6
yavetisyan <y.avetisyan88@gmail.com>
2022-03-28 10:45:27 +0400

    d

--- src/pages/auth/IndexAuth.tsx
+++ src/pages/auth/IndexAuth.tsx
@@ -1,24 +1,23 @@
 import React from "react";
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 import SignUp from "./SignUp";
 import AuthBackgroundVector from "../../assets/images/auth/AuthBackgroundVector";
 
 const IndexAuth = () => {
-    return (
-        <Box className="bg auth">
-            <div>
-                <SignUp/>
-            </div>
+  return (
+    <Box className="bg auth">
+      <div>
+        <SignUp />
+      </div>
 
-            <div className="testStyles">
-                <p></p>
-                <div className="childThing">
-                    <AuthBackgroundVector/>
-                </div>
-            </div>
-
-        </Box>
-    );
+      <div className="testStyles">
+        <p></p>
+        <div className="childThing">
+          <AuthBackgroundVector />
+        </div>
+      </div>
+    </Box>
+  );
 };
 
 export default IndexAuth;
--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,62 +1,78 @@
 import React from "react";
-import {Box, Button, FormControlLabel, FormGroup, TextField, Typography,} from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControlLabel,
+  FormGroup,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Checkbox from "@mui/material/Checkbox";
-import {useStyles} from "../../assets/scssInJS/signUp";
+import { useStyles } from "../../assets/scssInJS/signUp";
 import SignUpImg from "../../assets/images/auth/SignUpImg";
 
 const SignUp = () => {
-    const classes = useStyles()
+  const classes = useStyles();
 
-    return (
-        <Box className='auth'>
-            
-            <Box className="auth__title">
-                <Box component="div" className="auth__title-text">
-                    <Typography variant="h2" className={classes.authHeader}>
-                        Sign up
-                    </Typography>
-                    <Box>
-                        <Typography className={classes.authText}>
-                            Already have an account?
-                        </Typography>
+  return (
+    <Box className="auth">
+      <Box className="auth__title">
+        <Box component="div" className="auth__title-text">
+          <Typography variant="h2" className={classes.authHeader}>
+            Sign up
+          </Typography>
+          <Box>
+            <Typography className={classes.authText}>
+              Already have an account?
+            </Typography>
 
-                        <Button variant="text">Sign in</Button>
-                    </Box>
-                </Box>
-                <FormGroup>
-                    <Box className='auth__input'>
-                        <TextField label="Name / Surname" variant="outlined"
-                                   className='auth__input-box' size='small'
-                                   fullWidth/>
-                        <TextField label="Email" variant="outlined" type="email" fullWidth size='small'/>
-                        <TextField
-                            className={classes.authInput}
-                            label="Password (6+ charachter , 1 capital letter, 1 number)"
-                            type="password"
-                            variant="outlined"
-                            fullWidth
-                            size='small'
-                        />
-                    </Box>
-
-                    <FormControlLabel
-                        control={<Checkbox/>}
-                        label="Keep me signed in"
-                        value="checkbox"
-                        className={classes.authCheck}
-                    />
-
-                    <Button variant="contained" size='large'>Sign Up</Button>
-                </FormGroup>
-            </Box>
-
-            <Box className='auth__image-right'>
-                <SignUpImg/>
-
-            </Box>
-
+            <Button variant="text">Sign in</Button>
+          </Box>
         </Box>
-    );
+        <FormGroup>
+          <Box className="auth__input">
+            <TextField
+              label="Name / Surname"
+              variant="outlined"
+              className="auth__input-box"
+              size="small"
+              fullWidth
+            />
+            <TextField
+              label="Email"
+              variant="outlined"
+              type="email"
+              fullWidth
+              size="small"
+            />
+            <TextField
+              className={classes.authInput}
+              label="Password (6+ charachter , 1 capital letter, 1 number)"
+              type="password"
+              variant="outlined"
+              fullWidth
+              size="small"
+            />
+          </Box>
+
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Keep me signed in"
+            value="checkbox"
+            className={classes.authCheck}
+          />
+
+          <Button variant="contained" size="large">
+            Sign Up
+          </Button>
+        </FormGroup>
+      </Box>
+
+      <Box className="auth__image-right">
+        <SignUpImg />
+      </Box>
+    </Box>
+  );
 };
 
 export default SignUp;
