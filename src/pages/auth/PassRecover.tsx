f8d96ba84b0736e07849424bfee5582ff9b5cc6d
YuraAvetisyan <y.avetisyan88@gmail.com>
2022-03-29 22:55:44 +0400

    FEAT: add pasword reset

--- src/pages/auth/PassRecover.tsx
+++ src/pages/auth/PassRecover.tsx
@@ -1,21 +1,24 @@
-import React, {useState} from "react";
-import {Box, Button, FormGroup, Grid, Paper, TextField, Typography,} from "@mui/material";
-import {useStyles} from "../../assets/scssInJS/signUp";
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  FormGroup,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useStyles } from "../../assets/scssInJS/signUp";
 import PasswordRecovery from "../../assets/images/auth/PasswordRecovery";
-import {sendPasswordResetEmail} from "firebase/auth";
-import {auth} from "../../data/firebase";
-import {useNavigate} from "react-router-dom";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { auth } from "../../data/firebase";
+import { useNavigate } from "react-router-dom";
 
 const PassRecover = () => {
   const classes = useStyles();
   const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [error, setError] = useState(false);
-
-  const actionCodeSettings = {
-    url: "http://localhost:3000/sign-in",
-    handleCodeInApp: true,
-  };
+  const [email, setEmail] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
 
   const handleRecoverPass = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -24,24 +27,20 @@ const PassRecover = () => {
     await sendPasswordResetEmail(auth, email)
       .then((link) => {
         if (auth.config.authDomain) {
-          navigate('/password-recover-resend')
+          navigate("/password-recover-resend");
           setEmail("");
         }
       })
       .catch((error) => {
         setError(true);
-        alert('Enter email')
+        alert("Enter email");
       });
   };
 
   return (
     <div>
       <Box>
-        <Grid
-          className="auth authGrid"
-          container
-
-        >
+        <Grid className="auth authGrid" container>
           <Grid item lg={4} md={6} sm={12} xs={12}>
             <Paper className="auth__title ">
               <Box className="auth__passRecCheng">
@@ -62,7 +61,7 @@ const PassRecover = () => {
                 >
                   <TextField
                     className={classes.authInput}
-                    inputProps={{style: {fontSize: "14px"}}}
+                    inputProps={{ style: { fontSize: "14px" } }}
                     label="Email"
                     variant="outlined"
                     type="email"
@@ -83,16 +82,9 @@ const PassRecover = () => {
               </FormGroup>
             </Paper>
           </Grid>
-          <Grid
-            item
-            lg={4}
-            md={5}
-            sm={12}
-            xs={12}
-
-          >
+          <Grid item lg={4} md={5} sm={12} xs={12}>
             <Box className="auth__box-right ">
-              <PasswordRecovery/>
+              <PasswordRecovery />
             </Box>
           </Grid>
         </Grid>
