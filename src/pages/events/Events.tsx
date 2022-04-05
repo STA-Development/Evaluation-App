6770cfab2aec62757a0fbe92289df3f1b21fec1c
YuraAvetisyan <y.avetisyan88@gmail.com>
2022-04-05 23:50:10 +0400

    FEAT: add event checkout

--- src/pages/events/Events.tsx
+++ src/pages/events/Events.tsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import React from "react";
+import { Box, List } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { useEventsStyle } from "../../assets/styleJs/events/events";
+
+interface EventsLink {
+  id: number;
+  name: string;
+  route: string;
+}
 
 const Events = () => {
-    return (
-        <div>
-            Events
-        </div>
-    );
+  const classes = useEventsStyle();
+  const eventsSidebar: EventsLink[] = [
+    {
+      id: Math.random(),
+      name: "Events",
+      route: "/events",
+    },
+    {
+      id: Math.random(),
+      name: "Submissions",
+      route: "submissions",
+    },
+    {
+      id: Math.random(),
+      name: "Saved Submissions",
+      route: "saved_submissions",
+    },
+  ];
+  return (
+    <Box className="events">
+      <Box className="events__sidebar">
+        {eventsSidebar.map((link) => (
+          <NavLink
+            to={link.route}
+            key={link.id}
+            className={({ isActive }) =>
+              isActive ? "events__active" : "events__link"
+            }
+          >
+            <ListItem className={classes.listItem}>
+              <Box>
+                <ListItemText primary={link.name} />
+              </Box>
+            </ListItem>
+          </NavLink>
+        ))}
+      </Box>
+      <Box className="events__outlet">
+        <Outlet />
+      </Box>
+    </Box>
+  );
 };
 
 export default Events;
---
+++ src/pages/events/eventsInfo/Event.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Event = () => {
+  return <div>asd</div>;
+};
+
+export default Event;
