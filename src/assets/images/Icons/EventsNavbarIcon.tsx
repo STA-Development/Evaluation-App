53189ca91a76db66b69d261d107f1850f080711d
yavetisyan <y.avetisyan88@gmail.com>
2022-04-07 12:00:29 +0400

    FEAT: fixed issue

--- src/assets/images/Icons/EventsNavbarIcon.tsx
+++ src/assets/images/Icons/EventsNavbarIcon.tsx
@@ -9,45 +9,45 @@ const EventsNavbarIcon = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_2969_10025)">
+      <g clipPath="url(#clip0_2969_10025)">
         <path
           d="M19.375 1.25C19.375 0.875 19.125 0.625 18.75 0.625H1.25C0.875 0.625 0.625 0.875 0.625 1.25V18.75C0.625 19.125 0.875 19.375 1.25 19.375H18.75C19.125 19.375 19.375 19.125 19.375 18.75V1.25Z"
           stroke="white"
-          stroke-width="2"
-          stroke-miterlimit="10"
+          strokeWidth="2"
+          strokeMiterlimit="10"
         />
         <path
           d="M19.375 5H0.625"
           stroke="white"
-          stroke-width="2"
-          stroke-miterlimit="10"
+          strokeWidth="2"
+          strokeMiterlimit="10"
         />
         <path
           d="M3.75 3.75V6.25"
           stroke="white"
-          stroke-width="2"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M16.25 3.75V6.25"
           stroke="white"
-          stroke-width="2"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
-        <path d="M6.875 9.375H4.375V11.875H6.875V9.375Z" fill="white" />
-        <path d="M11.25 9.375H8.75V11.875H11.25V9.375Z" fill="white" />
-        <path d="M15.625 9.375H13.125V11.875H15.625V9.375Z" fill="white" />
-        <path d="M6.875 13.125H4.375V15.625H6.875V13.125Z" fill="white" />
-        <path d="M11.25 13.125H8.75V15.625H11.25V13.125Z" fill="white" />
-        <path d="M15.625 13.125H13.125V15.625H15.625V13.125Z" fill="white" />
+        <path d="M6.875 9.375H4.375V11.875H6.875V9.375Z" fill="white"/>
+        <path d="M11.25 9.375H8.75V11.875H11.25V9.375Z" fill="white"/>
+        <path d="M15.625 9.375H13.125V11.875H15.625V9.375Z" fill="white"/>
+        <path d="M6.875 13.125H4.375V15.625H6.875V13.125Z" fill="white"/>
+        <path d="M11.25 13.125H8.75V15.625H11.25V13.125Z" fill="white"/>
+        <path d="M15.625 13.125H13.125V15.625H15.625V13.125Z" fill="white"/>
       </g>
       <defs>
         <clipPath id="clip0_2969_10025">
-          <rect width="20" height="20" fill="white" />
+          <rect width="20" height="20" fill="white"/>
         </clipPath>
       </defs>
     </svg>
