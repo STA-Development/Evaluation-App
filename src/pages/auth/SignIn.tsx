780d169874698d5e3111b68879113bb2db132744
yavetisyan <y.avetisyan88@gmail.com>
2022-05-16 21:44:09 +0400

    FIX: fix axios signup

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -4,6 +4,7 @@ import {Link, useNavigate} from 'react-router-dom'
 import {
   Box,
   Button,
+  CircularProgress,
   FormControlLabel,
   FormGroup,
   Grid,
@@ -13,15 +14,16 @@ import {
 } from '@mui/material'
 import Checkbox from '@mui/material/Checkbox'
 import useStyles from '../../assets/styleJs/auth/signUp'
-import SignInImg from '../../assets/images/auth/SignInImg'
 import axiosInstance from '../../axiosInstance'
 import {afterSelf} from '../../utils/authUtils'
 import {setUser} from '../../redux/user/userSlice'
 import {useAppDispatch} from '../../redux/hooks'
+import SignInImg from '../../assets/images/auth/SignInImg'
 
 const SignIn = () => {
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
+  const [isAuth, setIsAuth] = useState<boolean>(false)
   const classes = useStyles()
   const globalClasses = useGlobalTheme()
   const [email, setEmail] = useState<string>('')
@@ -32,6 +34,7 @@ const SignIn = () => {
     e.preventDefault()
 
     try {
+      setIsAuth(true)
       const auth = await axiosInstance.post('/users/signIn', {
         email,
         password,
@@ -56,7 +59,9 @@ const SignIn = () => {
       if (auth.data) {
         navigate('/')
       }
+      setIsAuth(false)
     } catch (err) {
+      setIsAuth(false)
       setPassword('')
       setEmail('')
       setError(true)
@@ -68,65 +73,77 @@ const SignIn = () => {
       <Grid className="auth auth__grid" container>
         <Grid item lg={4} md={6} sm={12} xs={12}>
           <Paper className="auth__title ">
-            <Box className="auth__title-text">
-              <Typography variant="h2" className={classes.authHeader}>
-                Sign In
-              </Typography>
-              <Box>
-                <Typography className={classes.authText}>Already have an account?</Typography>
+            {!isAuth ? (
+              <>
+                <Box className="auth__title-text">
+                  <Typography variant="h2" className={classes.authHeader}>
+                    Sign In
+                  </Typography>
+                  <Box>
+                    <Typography className={classes.authText}>Already have an account?</Typography>
 
-                <Link to="/" className={classes.link}>
-                  Sign up now
+                    <Link to="/" className={classes.link}>
+                      Sign up now
+                    </Link>
+                  </Box>
+                </Box>
+                <FormGroup>
+                  <Box component="form" className="auth__input-box" onSubmit={handleSignIn}>
+                    <TextField
+                      className={classes.authInput}
+                      label="Email"
+                      variant="outlined"
+                      type="email"
+                      fullWidth
+                      size="small"
+                      error={error}
+                      value={email}
+                      autoComplete="email"
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setEmail(e.target.value)
+                      }
+                    />
+                    <TextField
+                      className={classes.authInput}
+                      label="Password"
+                      type="password"
+                      variant="outlined"
+                      fullWidth
+                      size="small"
+                      error={error}
+                      value={password}
+                      autoComplete="current-password"
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setPassword(e.target.value)
+                      }
+                    />
+
+                    <FormControlLabel
+                      control={<Checkbox />}
+                      label="Keep me signed in"
+                      value="checkbox"
+                      className={classes.authCheck}
+                    />
+
+                    <Button
+                      variant="contained"
+                      size="large"
+                      type="submit"
+                      className={globalClasses.button}
+                    >
+                      Sign In
+                    </Button>
+                  </Box>
+                </FormGroup>
+                <Link to="/password-recover-email" className="auth__forgot-pass">
+                  Forgot Password?
                 </Link>
+              </>
+            ) : (
+              <Box className="circle">
+                <CircularProgress />
               </Box>
-            </Box>
-            <FormGroup>
-              <Box component="form" className="auth__input-box" onSubmit={handleSignIn}>
-                <TextField
-                  className={classes.authInput}
-                  label="Email"
-                  variant="outlined"
-                  type="email"
-                  fullWidth
-                  size="small"
-                  error={error}
-                  value={email}
-                  autoComplete="email"
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-                />
-                <TextField
-                  className={classes.authInput}
-                  label="Password"
-                  type="password"
-                  variant="outlined"
-                  fullWidth
-                  size="small"
-                  error={error}
-                  value={password}
-                  autoComplete="current-password"
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-                />
-
-                <FormControlLabel
-                  control={<Checkbox />}
-                  label="Keep me signed in"
-                  value="checkbox"
-                  className={classes.authCheck}
-                />
-
-                <Button
-                  variant="contained"
-                  size="large"
-                  type="submit"
-                  className={globalClasses.button}
-                >
-                  Sign In
-                </Button>
-              </Box>
-            </FormGroup>
-            <Link to="/password-recover-email" className="auth__forgot-pass">
-              Forgot Password?
-            </Link>
+            )}
           </Paper>
         </Grid>
         <Grid item lg={4} md={5} sm={12} xs={12}>
