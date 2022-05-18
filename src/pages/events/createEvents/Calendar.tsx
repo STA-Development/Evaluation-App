5a16395570702f4df08e94101db38a396ab69d76
yavetisyan <y.avetisyan88@gmail.com>
2022-05-18 17:41:31 +0400

    FEAT: date

--- src/pages/events/createEvents/Calendar.tsx
+++ src/pages/events/createEvents/Calendar.tsx
@@ -1,33 +1,75 @@
 import React, {useState} from 'react'
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
+import {DatePicker, LocalizationProvider} from '@mui/lab'
+import AdapterDateFns from '@mui/lab/AdapterDateFns'
+import {Stack, TextField} from '@mui/material'
 
 const Calendar = () => {
-  const [startDate, setStartDate] = useState(new Date())
-  const [endDate, setEndDate] = useState(null)
-  const [startYear, setStartYear] = useState(new Date())
-  const onChange = (dates: any) => {
-    const [start, end] = dates
-    setStartDate(start)
-    setEndDate(end)
-  }
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
   return (
     <div>
-      <DatePicker
-        placeholderText={'Select Start / End Date'}
-        selected={startDate}
-        onChange={onChange}
-        startDate={startDate}
-        endDate={endDate}
-        selectsRange
-        monthsShown={2}
-      />
-      <DatePicker
-        selected={startYear}
-        onChange={(year: any) => setStartYear(year)}
-        showYearPicker
-        dateFormat="yyyy"
-      />
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Stack spacing={4} sx={{width: '250px'}}>
+          <DatePicker
+            label={'Date Picker'}
+            renderInput={(params) => <TextField {...params} />}
+            value={selectedDate}
+            onChange={(newValue) => {
+              setSelectedDate(newValue)
+            }}
+          />
+        </Stack>
+      </LocalizationProvider>
+
+      {/*<Box style={{width: '500px'}}>*/}
+      {/*  <DateRangePicker*/}
+      {/*    startText={'Check-in'}*/}
+      {/*    endText={'Check-out'}*/}
+      {/*    value={value}*/}
+      {/*    onChange={(newValue: any) => {*/}
+      {/*      setValue(newValue)*/}
+      {/*    }}*/}
+      {/*    renderInput={(startProps: any, endProps: any) => (*/}
+      {/*      <>*/}
+      {/*        <TextField {...startProps} />*/}
+      {/*        <Box sx={{mx: 2}}>to</Box>*/}
+      {/*        <TextField {...endProps} />*/}
+      {/*      </>*/}
+      {/*    )}*/}
+      {/*  />*/}
+      {/*</Box>*/}
+
+      {/*<Box>*/}
+      {/*  <div>*/}
+      {/*    <div>*/}
+      {/*      <div>Start Date</div>*/}
+      {/*      <div>End Date</div>*/}
+      {/*    </div>*/}
+      {/*    <div>chose date 2022</div>*/}
+      {/*  </div>*/}
+      {/*  <div>*/}
+      {/*    <div>*/}
+      {/*      <Button>Hunvar</Button>*/}
+      {/*      <Button>Petrvar</Button>*/}
+      {/*    </div>*/}
+      {/*    <div>*/}
+      {/*      <TextField*/}
+      {/*        name="someDate"*/}
+      {/*        label="Some Date"*/}
+      {/*        InputLabelProps={{shrink: true, required: false}}*/}
+      {/*        type="datetime-local"*/}
+      {/*        defaultValue={values.someDate}*/}
+      {/*        autoFocus*/}
+      {/*      />*/}
+      {/*      <TextField*/}
+      {/*        name="someDate"*/}
+      {/*        label="Some Date"*/}
+      {/*        InputLabelProps={{shrink: true, required: true}}*/}
+      {/*        type="date"*/}
+      {/*        defaultValue={values.someDate}*/}
+      {/*      />*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*</Box>*/}
     </div>
   )
 }
