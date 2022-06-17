8d4fda421a1a7fb72fd09a36d060c142dd6c04be
yavetisyan <y.avetisyan88@gmail.com>
2022-06-17 22:42:54 +0400

    FEAT: add chart

--- src/pages/reports/employeePersonalGrowthReport/AddEmployeeEvent.tsx
+++ src/pages/reports/employeePersonalGrowthReport/AddEmployeeEvent.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import {Box, Button, FormControl, Paper, TextField, Typography} from '@mui/material'
+import React, {useEffect, useState} from 'react'
+import {Box, Button, FormControl, List, Paper, TextField, Typography} from '@mui/material'
 import useCreateEventStyles from '../../../assets/styleJs/events/createEvent'
 import {IEmployeeReport} from '../../../types/EmpoyeeReport'
 
@@ -9,116 +9,119 @@ const AddEmployeeEvent = ({getReport}: {getReport: (value: IEmployeeReport) => v
   const [event, setEvent] = useState<string>('')
   const [position, setPosition] = useState<string>('')
   const [date, setDate] = useState<string>('')
-  const [employeeError, setEmployeeError] = useState<boolean>(false)
-  const [eventError, setEventError] = useState<boolean>(false)
+  const [error, setError] = useState<boolean>(false)
   const [employeeReport, setEmployeeReport] = useState<any>({})
+  const [animated, setAnimated] = useState<any>(false)
 
   const getApplyEmployee = () => {
-    setEmployeeError(false)
-    setEventError(false)
     if (employee && event) {
-      setEmployeeError(false)
-      setEventError(false)
       setEmployeeReport({
         employeeName: employee,
         employeePosition: position,
         eventName: event,
         evenDate: date,
       })
-    } else if (!employee) {
-      setEmployeeError(true)
-    } else if (!event) {
-      setEventError(true)
+      setError(false)
+      setAnimated(false)
+    } else {
+      setError(true)
+      setAnimated(true)
     }
   }
 
+  useEffect(() => {
+    getReport(employeeReport)
+  }, [employeeReport])
+
   return (
-    <Box className="employee-report personal-btn">
-      <Paper className="employee-paper">
-        <Box className="employee-paper__header">
-          <Typography>Employee *</Typography>
+    <Box className="add-employee-report">
+      <Box className={animated ? 'error-message' : ' error-message-out'}>
+        {error ? <List>Please enter value</List> : ''}
+      </Box>
+      <Box className="employee-report personal-btn">
+        <Paper className="employee-paper">
+          <Box className="employee-paper__header">
+            <Typography>Employee *</Typography>
+          </Box>
+          <FormControl className="employee-paper__form">
+            <TextField
+              InputLabelProps={{style: {fontSize: 14}}}
+              InputProps={{inputProps: {className: classesCreateEvent.inputText}}}
+              label="Name / Surname"
+              variant="outlined"
+              type="input"
+              autoComplete="off"
+              fullWidth
+              size="small"
+              name={employee}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setEmployee(e.target.value)
+              }}
+            />
+
+            <TextField
+              InputLabelProps={{style: {fontSize: 14}}}
+              InputProps={{inputProps: {className: classesCreateEvent.inputText}}}
+              label="Position"
+              variant="outlined"
+              type="input"
+              autoComplete="off"
+              fullWidth
+              size="small"
+              name={position}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setPosition(e.target.value)
+              }}
+            />
+          </FormControl>
+        </Paper>
+
+        <Paper className="employee-paper">
+          <Box className="employee-paper__header">
+            <Typography>Event</Typography>
+          </Box>
+          <FormControl className="employee-paper__form">
+            <TextField
+              InputLabelProps={{style: {fontSize: 14}}}
+              InputProps={{inputProps: {className: classesCreateEvent.inputText}}}
+              label="Event"
+              variant="outlined"
+              type="input"
+              autoComplete="off"
+              fullWidth
+              size="small"
+              name={event}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setEvent(e.target.value)
+              }}
+            />
+
+            <TextField
+              InputLabelProps={{style: {fontSize: 14}}}
+              InputProps={{inputProps: {className: classesCreateEvent.inputText}}}
+              label="Event Date"
+              variant="outlined"
+              type="input"
+              autoComplete="off"
+              fullWidth
+              size="small"
+              name={date}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setDate(e.target.value)
+              }}
+            />
+          </FormControl>
+        </Paper>
+        <Box className="employee-btn personal-btn">
+          <Button
+            variant="contained"
+            onClick={() => {
+              getApplyEmployee()
+            }}
+          >
+            APPLY
+          </Button>
         </Box>
-        <FormControl className="employee-paper__form">
-          <TextField
-            InputLabelProps={{style: {fontSize: 14}}}
-            InputProps={{inputProps: {className: classesCreateEvent.inputText}}}
-            label="Name / Surname"
-            variant="outlined"
-            type="input"
-            error={employeeError}
-            autoComplete="off"
-            fullWidth
-            size="small"
-            name={employee}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setEmployee(e.target.value)
-            }}
-          />
-
-          <TextField
-            InputLabelProps={{style: {fontSize: 14}}}
-            InputProps={{inputProps: {className: classesCreateEvent.inputText}}}
-            label="Position"
-            variant="outlined"
-            type="input"
-            autoComplete="off"
-            fullWidth
-            size="small"
-            name={position}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setPosition(e.target.value)
-            }}
-          />
-        </FormControl>
-      </Paper>
-
-      <Paper className="employee-paper">
-        <Box className="employee-paper__header">
-          <Typography>Event</Typography>
-        </Box>
-        <FormControl className="employee-paper__form">
-          <TextField
-            InputLabelProps={{style: {fontSize: 14}}}
-            InputProps={{inputProps: {className: classesCreateEvent.inputText}}}
-            label="Event"
-            variant="outlined"
-            type="input"
-            error={eventError}
-            autoComplete="off"
-            fullWidth
-            size="small"
-            name={event}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setEvent(e.target.value)
-            }}
-          />
-
-          <TextField
-            InputLabelProps={{style: {fontSize: 14}}}
-            InputProps={{inputProps: {className: classesCreateEvent.inputText}}}
-            label="Event Date"
-            variant="outlined"
-            type="input"
-            autoComplete="off"
-            fullWidth
-            size="small"
-            name={date}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setDate(e.target.value)
-            }}
-          />
-        </FormControl>
-      </Paper>
-      <Box className="employee-btn personal-btn">
-        <Button
-          variant="contained"
-          onClick={() => {
-            getApplyEmployee()
-            getReport(employeeReport)
-          }}
-        >
-          APPLY
-        </Button>
       </Box>
     </Box>
   )
--- src/pages/reports/employeePersonalGrowthReport/EmployeePersonalGrowthReport.tsx
+++ src/pages/reports/employeePersonalGrowthReport/EmployeePersonalGrowthReport.tsx
@@ -1,11 +1,145 @@
-import React, {useState} from 'react'
-import {Box} from '@mui/material'
+import React, {useEffect, useState} from 'react'
+import {Box, CircularProgress} from '@mui/material'
 import AddEmployeeEvent from './AddEmployeeEvent'
 import {IEmployeeReport} from '../../../types/EmpoyeeReport'
+import HighchartsReact from 'highcharts-react-official'
+import Highcharts from 'highcharts'
+import {color} from '../../../assets/style/globalVariables'
+import {randomColor, randomData} from '../../../utils/utils'
 
 const EmployeePersonalGrowthReport = () => {
   const [employeeReport, setEmployeeReport] = useState<IEmployeeReport>()
-  console.log(employeeReport)
+  const [isUpdatingChart, setIsUpdatingChart] = useState<boolean>(true)
+  const [chart, setChart] = useState<HighchartsReact.Props>()
+
+  useEffect(() => {
+    if (employeeReport?.employeeName && employeeReport?.eventName) {
+      setIsUpdatingChart(true)
+      setChart({
+        chart: {
+          type: 'column',
+          height: '600px',
+          marginRight: 75,
+          marginLeft: 75,
+        },
+
+        credits: {
+          enabled: false,
+        },
+
+        title: {
+          text: '',
+        },
+
+        subtitle: {
+          text: '',
+        },
+
+        legend: {
+          alignColumns: true,
+          align: 'left',
+          layout: 'horizontal',
+          verticalAlign: 'top',
+          x: 30,
+          useHTML: 'Score',
+          title: {
+            text: 'Score',
+            style: {fontWeight: 'bold', color: color.employeeColor},
+          },
+        },
+
+        xAxis: {
+          categories: ['1st Quarter', '2st Quarter', '3st Quarter'],
+          labels: {
+            style: {
+              color: '#756F86',
+              fontSize: '14px',
+            },
+          },
+          margin: 0,
+          gridLineColor: '#DBE2EA',
+          gridLineWidth: 2,
+          lineColor: 'transparent',
+          max: 2,
+        },
+
+        yAxis: {
+          min: 0,
+          max: 10,
+
+          title: {
+            text: '',
+          },
+          gridLineDashStyle: 'LongDash',
+          lineColor: '#fff',
+          lineWidth: 20,
+        },
+        colors: [randomColor(), randomColor(), randomColor()],
+        series: [
+          {
+            name: 'Work Performance',
+            data: [randomData(), randomData(), randomData()],
+          },
+          {
+            name: 'Personal Skills ',
+            data: [randomData(), randomData(), randomData()],
+          },
+          {
+            name: 'Company Culture',
+            data: [randomData(), randomData(), randomData()],
+          },
+        ],
+        tooltip: {
+          headerFormat: '<span style="font-size:10px">{point.key} </span><table>',
+          pointFormat:
+            '<tr><td style="color:{series.color};padding:0">{series.name}:  </td>' +
+            '<td style="padding:0"><b> {point.y:.1f}</b></td></tr>',
+          footerFormat: '</table>',
+          shared: false,
+          useHTML: false,
+        },
+
+        responsive: {
+          rules: [
+            {
+              condition: {
+                maxWidth: 500,
+                padding: 50,
+              },
+              chartOptions: {
+                legend: {
+                  align: 'center',
+                  verticalAlign: 'top',
+                  layout: 'horizontal',
+                },
+                yAxis: {
+                  labels: {
+                    align: 'left',
+                    x: 0,
+                    y: -5,
+                  },
+                  title: {
+                    text: null,
+                  },
+                },
+                subtitle: {
+                  text: null,
+                },
+                credits: {
+                  enabled: false,
+                },
+              },
+            },
+          ],
+        },
+      })
+    }
+  }, [employeeReport])
+
+  useEffect(() => {
+    setIsUpdatingChart(false)
+  }, [chart])
+
   return (
     <Box>
       <AddEmployeeEvent
@@ -13,6 +147,14 @@ const EmployeePersonalGrowthReport = () => {
           setEmployeeReport(item)
         }}
       />
+
+      <Box>
+        {isUpdatingChart ? (
+          <CircularProgress />
+        ) : (
+          <HighchartsReact highcharts={Highcharts} options={chart} />
+        )}
+      </Box>
     </Box>
   )
 }
